83841973ce09abfbc4d81a3ed3fd176dc407f0ce
philswiftcoding <philswift@gmx.com>
2023-03-22 09:55:25 -0700

    field centric

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tele/FieldCentricMecanum.java
@@ -0,0 +1,91 @@
+package org.firstinspires.ftc.teamcode.tele;
+
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.hardware.bosch.JustLoggingAccelerationIntegrator;
+
+@TeleOp(name="FieldCentric")
+public class FieldCentricMecanum extends LinearOpMode {
+
+    double multiplier = 1.0;
+    BNO055IMU imu;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        BNO055IMU.Parameters parameters = new BNO055IMU.Parameters();
+        parameters.angleUnit           = BNO055IMU.AngleUnit.DEGREES;
+        parameters.accelUnit           = BNO055IMU.AccelUnit.METERS_PERSEC_PERSEC;
+        parameters.calibrationDataFile = "BNO055IMUCalibration.json"; // see the calibration sample opmode
+        parameters.loggingEnabled      = true;
+        parameters.loggingTag          = "IMU";
+        parameters.accelerationIntegrationAlgorithm = new JustLoggingAccelerationIntegrator();
+
+        imu = hardwareMap.get(BNO055IMU.class, "imu");
+        imu.initialize(parameters);
+
+        // FTC Dashboard Setups
+        FtcDashboard dashboard = FtcDashboard.getInstance();
+        TelemetryPacket packet = new TelemetryPacket();
+
+        // Declare our motors
+        // Make sure your ID's match your configuration
+        DcMotor motorFrontLeft = hardwareMap.dcMotor.get("motorFrontLeft");
+        DcMotor motorBackLeft = hardwareMap.dcMotor.get("motorBackLeft");
+        DcMotor motorFrontRight = hardwareMap.dcMotor.get("motorFrontRight");
+        DcMotor motorBackRight = hardwareMap.dcMotor.get("motorBackRight");
+
+        // Reverse the right side motors
+        motorFrontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorBackRight.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        telemetry.addLine("Ready");
+        telemetry.update();
+
+        waitForStart();
+
+        if (isStopRequested()) return;
+
+        while (opModeIsActive()) {
+            double angle = Math.toRadians(imu.getAngularOrientation().firstAngle);
+
+            if(gamepad1.left_trigger > 0.2 || gamepad1.right_trigger > 0.2 || gamepad1.left_bumper || gamepad1.right_bumper) {
+                multiplier = 0.5;
+            }
+            else {
+                multiplier = 1.0;
+            }
+
+            // Gamepad Inputs
+            double y = -gamepad1.left_stick_y;
+            double x = gamepad1.left_stick_x * 1.1;
+            double rx = gamepad1.right_stick_x;
+
+            double rotx = x*Math.cos(-angle) - y*Math.sin(-angle);
+            double roty = x*Math.sin(-angle) + y*Math.cos(-angle);
+
+            double denominator = Math.max(Math.abs(roty) + Math.abs(rotx) + Math.abs(rx), 1);
+
+            double frontRightPower = (roty + rotx + rx) / denominator;
+            double backRightPower = (roty - rotx + rx) / denominator;
+            double frontLeftPower = (roty - rotx - rx) / denominator;
+            double backLeftPower = (roty + rotx - rx) / denominator;
+
+
+            packet.put("frontLeftPower", frontLeftPower);
+            packet.put("frontRightPower", frontRightPower);
+            packet.put("backLeftPower", backLeftPower);
+            packet.put("backRightPower", backRightPower);
+
+            motorFrontLeft.setPower(frontLeftPower);
+            motorBackLeft.setPower(backLeftPower);
+            motorFrontRight.setPower(frontRightPower);
+            motorBackRight.setPower(backRightPower);
+        }
+    }
+}
