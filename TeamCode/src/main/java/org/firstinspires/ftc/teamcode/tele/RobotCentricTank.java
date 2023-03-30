c6f8f9e97954e343603e2f9e8be5f4f817d82036
philswiftcoding <philswift@gmx.com>
2023-03-29 20:04:18 -0700

    fixed lift in teleop

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tele/RobotCentricTank.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/tele/RobotCentricTank.java
@@ -5,7 +5,6 @@ import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.DistanceSensor;
@@ -23,20 +22,18 @@ public class RobotCentricTank extends LinearOpMode {
 
     public static double Kp = 0.05, Ki = 0.0, Kd = 0.0;
     public static double targetInches = 0.0;
-    public static double low = 1.0;
-    public static double medium = 11.0;
-    public static double high = 21.0;
+    public static double low = 1.0 * 30.71283;
+    public static double medium = 11.0 * 30.71283;
+    public static double high = 21.0 * 30.71283;
     public static double open = 0.8;
     public static double closed = 1.0;
     double multiplier = 1.0;
     double stackHeight = 0.0;
 
-    NormalizedColorSensor clawSensor;
+    //NormalizedColorSensor clawSensor;
     //NormalizedColorSensor guideSensor;
-    DistanceSensor clawDistanceSensor;
+    //DistanceSensor clawDistanceSensor;
     //DistanceSensor guideDistanceSensor;
-
-    DcMotorEx liftMotorLeft;
 
     FtcDashboard dashboard = FtcDashboard.getInstance();
     Telemetry dashboardTelemetry = dashboard.getTelemetry();
@@ -51,7 +48,7 @@ public class RobotCentricTank extends LinearOpMode {
         TelemetryPacket packet = new TelemetryPacket();
 
         // Servo
-        Servo gripServo = hardwareMap.servo.get("manipulator");
+        //Servo gripServo = hardwareMap.servo.get("manipulator");
         //Servo leftV4B = hardwareMap.servo.get("leftV4B");
         //Servo rightV4B = hardwareMap.servo.get("rightV4B");
         //Servo leftGuide = hardwareMap.servo.get("leftGuide");
@@ -59,23 +56,27 @@ public class RobotCentricTank extends LinearOpMode {
 
         // Declare our motors
         // Make sure your ID's match your configuration
-        liftMotorLeft = hardwareMap.get(DcMotorEx.class, "liftMotorLeft");
-        //DcMotor liftMotorLeft = hardwareMap.dcMotor.get("liftMotorLeft");
-        //DcMotor liftMotorRight = hardwareMap.dcMotor.get("liftMotorRight");
+        DcMotor liftMotorLeft = hardwareMap.dcMotor.get("liftMotorLeft");
+        DcMotor liftMotorRight = hardwareMap.dcMotor.get("liftMotorRight");
 
-        //DcMotor driveMotorLeft = hardwareMap.dcMotor.get("driveMotorLeft");
-        //DcMotor driveMotorRight = hardwareMap.dcMotor.get("driveMotorRight");
+        liftMotorLeft.setTargetPosition(0);
+        liftMotorRight.setTargetPosition(0);
+        liftMotorLeft.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        liftMotorRight.setMode(DcMotor.RunMode.RUN_TO_POSITION);
 
-        //driveMotorLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        //liftMotorRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        DcMotor driveMotorLeft = hardwareMap.dcMotor.get("driveMotorLeft");
+        DcMotor driveMotorRight = hardwareMap.dcMotor.get("driveMotorRight");
 
-        clawSensor = hardwareMap.get(NormalizedColorSensor.class, "clawSensor");
+        driveMotorLeft.setDirection(DcMotorSimple.Direction.REVERSE);
+        liftMotorRight.setDirection(DcMotorSimple.Direction.REVERSE);
+
+        //clawSensor = hardwareMap.get(NormalizedColorSensor.class, "clawSensor");
         //guideSensor = hardwareMap.get(NormalizedColorSensor.class, "guideSensor");
-        clawDistanceSensor = hardwareMap.get(DistanceSensor.class, "clawSensor");
+        //clawDistanceSensor = hardwareMap.get(DistanceSensor.class, "clawSensor");
         //guideDistanceSensor = hardwareMap.get(DistanceSensor.class, "guideSensor");
-        clawSensor.setGain(10);
+        //clawSensor.setGain(10);
 
-        gripServo.setPosition(open);
+        //gripServo.setPosition(open);
 
         telemetry.addLine("Ready");
         telemetry.update();
@@ -85,17 +86,18 @@ public class RobotCentricTank extends LinearOpMode {
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
-            NormalizedRGBA clawColors = clawSensor.getNormalizedColors();
+            double position = (liftMotorLeft.getCurrentPosition() + liftMotorRight.getCurrentPosition()) / (30.71283 / 2);
+            //NormalizedRGBA clawColors = clawSensor.getNormalizedColors();
             //NormalizedRGBA guideColors = guideSensor.getNormalizedColors();
-            double clawDistance = clawDistanceSensor.getDistance(DistanceUnit.MM);
+            //double clawDistance = clawDistanceSensor.getDistance(DistanceUnit.MM);
             //double guideDistance = guideDistanceSensor.getDistance(DistanceUnit.MM);
-            if(clawColors.red > 0.2 && clawDistance < 20 || clawColors.blue > 0.2 && clawDistance < 20) {
-                gripServo.setPosition(closed);
-                telemetry.addData("Cone Detected", clawDistance);
-            }
-            else {
-                gripServo.setPosition(open);
-            }
+            //if(clawColors.red > 0.2 && clawDistance < 20 || clawColors.blue > 0.2 && clawDistance < 20) {
+                //gripServo.setPosition(closed);
+                //telemetry.addData("Cone Detected", clawDistance);
+            //}
+            //else {
+                //gripServo.setPosition(open);
+            //}
 
             //if(guideColors.red > 0.9 && guideColors.green > 0.9  && guideColors.blue < 0.1 && guideDistance < 10 && targetInches >= 1) {
                 //targetInches = targetInches - 1;
@@ -104,93 +106,105 @@ public class RobotCentricTank extends LinearOpMode {
             if(gamepad1.left_trigger > 0.2) {
                 multiplier = 0.5;
             }
-            else if(targetInches < 10) {
-                multiplier = 0.75;
+            else if(position < 12) {
+                multiplier = 0.5;
             }
             else {
                 multiplier = 1.0;
             }
 
             // Release cone
-            if (gamepad1.x) {
-                if (targetInches >= 1) {
-                    targetInches = targetInches - 1;
-                    gripServo.setPosition(open);
+            //if (gamepad1.x) {
+                //if (targetInches >= 1) {
+                    //targetInches = targetInches - 1;
+                    //gripServo.setPosition(open);
                     //leftV4B.setPosition(0.0);
                     //rightV4B.setPosition(0.83);
                     //leftGuide.setPosition(0.0);
                     //rightGuide.setPosition(0.3);
-                    targetInches = 0.0;
-                } else {
-                    gripServo.setPosition(open);
-                }
-            }
+                    //targetInches = 0.0;
+                //}
+                //else {
+                    //gripServo.setPosition(open);
+                //}
+            //}
             // Manual claw
-            if (gamepad1.dpad_right) {
-                gripServo.setPosition(closed);
-            }
+            //if (gamepad1.dpad_right) {
+                //gripServo.setPosition(closed);
+            //}
 
             // Guide
-            if (gamepad1.dpad_left) {
+            //if (gamepad1.dpad_left) {
                 //leftGuide.setPosition(0.0);
                 //rightGuide.setPosition(0.3);
-            } else if (gamepad1.dpad_up) {
+            //} else if (gamepad1.dpad_up) {
                 //leftGuide.setPosition(0.3);
                 //rightGuide.setPosition(0.0);
-            }
+            //}
 
             // Auto heights
             if (gamepad1.y) {
-                gripServo.setPosition(closed);
-                targetInches = high;
+                liftMotorLeft.setTargetPosition((int) high);
+                liftMotorLeft.setPower(1.0);
+                liftMotorRight.setTargetPosition((int) high);
+                liftMotorRight.setPower(1.0);
+                //gripServo.setPosition(closed);
+                //targetInches = high;
                 //leftV4B.setPosition(0.83);
                 //rightV4B.setPosition(0.0);
                 //leftGuide.setPosition(0.0);
                 //rightGuide.setPosition(0.3);
-            } else if (gamepad1.b) {
-                gripServo.setPosition(closed);
-                targetInches = medium;
+            }
+            else if (gamepad1.b) {
+                liftMotorLeft.setTargetPosition((int) medium);
+                liftMotorLeft.setPower(1.0);
+                liftMotorRight.setTargetPosition((int) medium);
+                liftMotorRight.setPower(1.0);
+                //gripServo.setPosition(closed);
+                //targetInches = medium;
                 //leftV4B.setPosition(0.83);
                 //rightV4B.setPosition(0.0);
                 //leftGuide.setPosition(0.0);
                 //rightGuide.setPosition(0.3);
-            } else if (gamepad1.a) {
-                gripServo.setPosition(closed);
-                targetInches = low;
+            }
+            else if (gamepad1.a) {
+                liftMotorLeft.setTargetPosition((int) low);
+                liftMotorLeft.setPower(1.0);
+                liftMotorRight.setTargetPosition((int) low);
+                liftMotorRight.setPower(1.0);
+                //gripServo.setPosition(closed);
                 //leftV4B.setPosition(0.83);
                 //rightV4B.setPosition(0.0);
                 //leftGuide.setPosition(0.0);
                 //rightGuide.setPosition(0.3);
-            } else if (gamepad1.dpad_down) {
-                gripServo.setPosition(open);
-                targetInches = 0.0;
+            }
+            else if (gamepad1.dpad_down) {
+                liftMotorLeft.setTargetPosition(0);
+                liftMotorLeft.setPower(1.0);
+                liftMotorRight.setTargetPosition(0);
+                liftMotorRight.setPower(1.0);
+                //gripServo.setPosition(open);
                 //leftV4B.setPosition(0.0);
                 //rightV4B.setPosition(0.83);
                 //leftGuide.setPosition(0.3);
                 //rightGuide.setPosition(0.0);
             }
             // Cone stack heights
-            if (gamepad1.right_trigger > 0.2 && targetInches >= 1) {
+            if (gamepad1.right_bumper && stackHeight >= 1) {
                 stackHeight = stackHeight - 1;
-                targetInches = stackHeight;
+                liftMotorLeft.setTargetPosition((int) (stackHeight * 30.71283));
+                liftMotorLeft.setPower(1.0);
+                liftMotorRight.setTargetPosition((int) (stackHeight * 30.71283));
+                liftMotorRight.setPower(1.0);
             }
 
-            if (gamepad1.right_bumper) {
+            if (gamepad1.left_bumper) {
                 stackHeight = stackHeight + 1;
-                targetInches = stackHeight;
+                liftMotorLeft.setTargetPosition((int) (stackHeight * 30.71283));
+                liftMotorLeft.setPower(1.0);
+                liftMotorRight.setTargetPosition((int) (stackHeight * 30.71283));
+                liftMotorRight.setPower(1.0);
             }
-
-            int targetPosition = (int)(targetInches * 30.71283);
-            // Update pid controller
-            double command = control.update(targetPosition, liftMotorLeft.getCurrentPosition());
-            command = Range.clip(command, -1, 1);
-            // Assign PID output
-            double motorPosition = liftMotorLeft.getCurrentPosition();
-            telemetry.addData("Target Position", targetPosition);
-            telemetry.addData("Command Power", command);
-            telemetry.addData("Target Inches", targetInches);
-            telemetry.addData("Motor Position", motorPosition);
-            liftMotorLeft.setPower(command);
 
             // drivePower is the power for forward/backward movement
             // rotatePower is the power for rotating the robot
@@ -198,8 +212,8 @@ public class RobotCentricTank extends LinearOpMode {
             float rotatePower = gamepad1.right_stick_x;
 
             // Flip these signs if the robot rotates the wrong way
-            //driveMotorLeft.setPower((drivePower + rotatePower) * multiplier);
-            //driveMotorRight.setPower((drivePower - rotatePower) * multiplier);
+            driveMotorLeft.setPower((drivePower + rotatePower) * multiplier);
+            driveMotorRight.setPower((drivePower - rotatePower) * multiplier);
 
             telemetry.update();
             dashboardTelemetry.update();
